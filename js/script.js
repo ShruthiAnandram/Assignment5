69e8df4807df22c313ab78c6fcd6c3a9f37a7902
shruthi <shruthisa.17@gmail.com>
2023-07-21 15:47:08 -0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let form;
+let empId;
+let empName;
+let empExtn;
+let email;
+let dept;
+ 
+// On load event. defer keyword in <script> in html helps
+window.addEventListener('load', (e) => {
+        console.log("Loading");
+});    
+  
+// Helper function 
+const $ = function (id) {
+    return document.getElementById(id);
+};
+
+
+ // Event Listener attached to Form. It fires on Form submission    
+ $('form').addEventListener('submit', function(e){
+       
+      e.preventDefault();    //stop form from submitting
+       
+      // Get DOM Elements  
+      form = $('form').value;
+      empId = $('empId').value;
+      empName = $('empName').value;
+      empExtn = $('empExtn').value;   
+      email = $('email').value;
+      dept = $('dept').value;
+    
+      console.log(`ID: ${empId}` + "\n" +
+                   `Name: ${empName}` + "\n" + 
+                   `Extn: ${empExtn}` + "\n" + 
+                   `Email: ${email}` + "\n" + 
+                   `Dept: ${dept}`);     
+  });
+      
